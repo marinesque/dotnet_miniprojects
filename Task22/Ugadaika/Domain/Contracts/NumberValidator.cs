9444606100c29d21ea9d21adb8c4e1599a2d5095
marinesque <BigOldTurtle>
2025-05-06 21:05:27 +0200

    Task22 part 1

---
+++ Task22/Ugadaika/Domain/Contracts/NumberValidator.cs
@@ -0,0 +1,12 @@
+﻿using Ugadaika.Domain.Interfaces;
+
+namespace Ugadaika.Domain.Contracts
+{
+    internal class NumberValidator : INumberValidator
+    {
+        public bool Validate(int curNumber, int minNumber, int maxNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task22/Ugadaika/Domain/Contracts/UgadaikaGame.cs
@@ -0,0 +1,26 @@
+﻿using Ugadaika.Domain.Interfaces;
+
+namespace Ugadaika.Domain.Contracts
+{
+    public class UgadaikaGame : INumberGenerator, INumberValidator
+    {
+        public INumberGenerator _generator;
+        private NumberPair _numberPair;
+
+        public UgadaikaGame(INumberGenerator generator, NumberPair numberPair)
+        {
+            _generator = generator;
+            _numberPair = numberPair;
+        }
+
+        public int Generate(int minNumber, int maxNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(int curNumber, int minNumber, int maxNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
